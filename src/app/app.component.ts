77cfc8a65d0392e5b3d627a581dec110621b3180
khalidBoughaba <boughaba.khalid.dev@gmail.com>
2024-06-30 16:21:05 +0100

    p-216

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  DoCheck,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
+import { BehaviorSubject, Subscription, interval, map } from 'rxjs';
 
+interface user {
+  name: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent implements OnInit {
-  title = 'ngTraining';
+export class AppComponent implements OnInit, DoCheck {
+  time!: number;
+  timeSubscription!: Subscription;
 
-  constructor() {}
+  @ViewChild('h1', { static: false }) h1!: ElementRef;
 
-  ngOnInit(): void {}
+  constructor(private ref: ChangeDetectorRef) {
+    console.log(this.ref);
+    this.ref.detach();
+  }
+  ngOnInit(): void {
+    // interval(10)
+    //   .pipe(map((time) => time * 10))
+    //   .subscribe((time) => {
+    //     this.time = time;
+    //     if (this.time % 1000 === 0) {
+    //       this.ref.detectChanges();
+    //     }
+    //   });
+  }
+
+  ngDoCheck(): void {
+    //console.log(this.time);
+  }
 }
